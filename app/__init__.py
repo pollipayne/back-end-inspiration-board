e88c6e57432943604a2469d9bd57f393f26fd326
Lauren <lacovington16@gmail.com>
2021-06-28 18:06:28 -0700

    re-submitting branch

--- app/__init__.py
+++ app/__init__.py
@@ -26,10 +26,11 @@ def create_app():
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    # from .routes import example_bp
-    # app.register_blueprint(example_bp)
     from .routes import card_bp
     app.register_blueprint(card_bp)
+
+    from .routes import board_bp
+    app.register_blueprint(board_bp)
 
     CORS(app)
     return app
--- app/models/board.py
+++ app/models/board.py
@@ -1,8 +1,14 @@
 from app import db
 
-
 class Board(db.Model):
-    __tablename__ = 'board'
+    __name__ = 'board'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
+
+    def format_to_json(self):
+        return {
+                "id": self.id, 
+                "title": self.title,
+                "owner": self.owner
+            }
