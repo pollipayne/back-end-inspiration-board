790f650dcfada6f15f2f20af9c6a269d4ddd5ea0
Brittney Payne <ohoptional@gmail.com>
2021-06-29 13:43:21 -0700

    forgot to add a board_id attribute to card objects.

--- app/models/card.py
+++ app/models/card.py
@@ -15,7 +15,8 @@ class Card(db.Model):
         card_dictionary = {
             'id': self.id, 
             "message": self.message, 
-            "likes_count": self.likes_count
+            "likes_count": self.likes_count,
+            "board_id": self.board_id
         }
 
         return card_dictionary
